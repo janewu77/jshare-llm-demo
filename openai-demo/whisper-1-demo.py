4daa306e4f9788e892a5603218a35a5fa3298581
Jane wu <conchwu@hotmail.com>
2023-03-10 11:39:18 +0800

    update： whisper-1 translate

--- openai-demo/whisper-1-demo.py
+++ openai-demo/whisper-1-demo.py
@@ -4,14 +4,31 @@ from datetime import datetime
 from dateutil import rrule
 
 
+# Whisper-1的例子
 def demo_whisper_1_run():
-    start_time = datetime.now()
+    # 音频转文字
 
-    # file = open("resources/w1-cn.mp3", "rb")
-    file = open("../resources/w1-en.mp3", "rb")
-    transcription = openai.Audio.transcribe("whisper-1", file)
-    file.close()
+    start_time = datetime.now()
+    # audio_file = open("../resources/w1-cn.mp3", "rb")
+    audio_file = open("../resources/w1-en.mp3", "rb")
+    transcription = openai.Audio.transcribe("whisper-1", audio_file)
+    audio_file.close()
     print(transcription.get("text"))
+
+    seconds = rrule.rrule(freq=rrule.SECONDLY, dtstart=start_time, until=datetime.now())
+    print(f"total spend: {seconds.count()} seconds")
+    print('【END】.')
+
+
+def demo_whisper_2_run():
+    # 翻译，目前仅支持翻成英文
+    # translate: cn -> english
+
+    start_time = datetime.now()
+    audio_file = open("../resources/w1-cn.mp3", "rb")
+    transcript = openai.Audio.translate("whisper-1", audio_file)
+    audio_file.close()
+    print(transcript.get("text"))
 
     seconds = rrule.rrule(freq=rrule.SECONDLY, dtstart=start_time, until=datetime.now())
     print(f"total spend: {seconds.count()} seconds")
