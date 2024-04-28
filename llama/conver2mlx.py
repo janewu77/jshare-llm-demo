a8c3c33aec46207298eee126723a3e684e469f2b
janewu77 <conchwu@hotmail.com>
2024-04-29 01:15:00 +0800

    update: convert to mlx

--- llama/conver2mlx.py
+++ llama/conver2mlx.py
@@ -1,8 +1,9 @@
 from mlx_lm import convert
 
-# 把Llama3-8B-Chinese-Chat 生成mlx的格式
+# 生成mlx的格式
 # model_source = "shenzhi-wang/Llama3-8B-Chinese-Chat"
-model_source = "/Users/jingwu/janewu/llm-model/llama/Llama3-8B-Chinese-Chat"
-mlx_path = "mlx/Llama3-8B-Chinese-Chat"
+# model_source = "/Users/jingwu/janewu/llm-model/llama/Llama3-8B-Chinese-Chat"
+model_source = "/Users/jingwu/janewu/llm-model/llama/meta/Meta-Llama-3-8B-Instruct"
+mlx_path = "mlx/Meta-Llama-3-8B-Instruct"
 convert(model_source, mlx_path, quantize=True)
 
--- llama/demo_llama3-70B-I-4bit-mlx-lm.py
+++ llama/demo_llama3-70B-I-4bit-mlx-lm.py
@@ -2,12 +2,18 @@ from mlx_lm import load, generate
 
 # myenv: mlx-lm-310
 # https://huggingface.co/mlx-community/Meta-Llama-3-70B-Instruct-4bit
-# 2024.04.22 it's buggy
+# 2024.04.22 download it's buggy
+
 
 model_path = "/Users/jingwu/janewu/llm-model/llama/mlx/Meta-Llama-3-70B-Instruct-4bit"
 # model_path = "/Users/jingwu/janewu/llm-model/llama/mlx/Meta-Llama-3-8B-Instruct-4bit"
+
 # convert from "shenzhi-wang/Llama3-8B-Chinese-Chat"
 # model_path = "/Users/jingwu/janewu/llm-model/llama/mlx/Meta-Llama3-8B-Chinese-Chat"
+
+# convert from "/Users/jingwu/janewu/llm-model/llama/meta/Meta-Llama-3-8B-Instruct"
+# model_path = "/Users/jingwu/janewu/llm-model/llama/mlx/jw/Meta-Llama-3-8B-Instruct"
+
 
 model, tokenizer = load(model_path)
 
@@ -15,7 +21,7 @@ model, tokenizer = load(model_path)
 # prompt = "买了一只股票，先涨了10%，再降10%，最终的盈亏情况如何？请给出具体计算过程和结论。think step by step"
 prompt = "Write a story about Einstein"
 
-response = generate(model, tokenizer, prompt=prompt, verbose=True) # , max_tokens=512) # temp=0.0
+response = generate(model, tokenizer, prompt=prompt, verbose=True)  # , max_tokens=512) # temp=0.0
 
 # 70B-Instruct-4bit
 # Prompt: 0.851 tokens-per-sec
@@ -28,3 +34,7 @@ response = generate(model, tokenizer, prompt=prompt, verbose=True) # , max_token
 # Meta-Llama3-8B-Chinese-Chat
 # Prompt: 40.995 tokens-per-sec
 # Generation: 67.381 tokens-per-sec
+
+# JW/Meta-Llama-3-8B-Instruct
+# Prompt: 35.422 tokens-per-sec
+# Generation: 66.475 tokens-per-sec
